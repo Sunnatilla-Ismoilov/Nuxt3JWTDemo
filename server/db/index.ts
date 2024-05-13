e959e97de24607b1edbbc022a93e510e53cc7cb0
Sunnatilla <sunnatilla.ismailov@gmail.com>
2024-05-13 05:39:26 +0300

    updated db configurations

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,12 +1,13 @@
 import { Nitro } from "nitropack";
 import mongoose from "mongoose";
+import { config } from "dotenv";
 
 export default async (_nitroApp: Nitro) => {
-  const config = useRuntimeConfig();
+  config();
   try {
-    await mongoose.connect(config.MONGODB_URI);
-    return { statusCode: 201 };
+    await mongoose.connect(process.env.MONGODB_URI);
+    return { statusCode: 201, message: "Connected to MongoDB!" };
   } catch (error) {
-    return { statusCode: 500 };
+    return { statusCode: 500, message: "Internal Server Error" };
   }
 };
