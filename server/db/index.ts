6885fb014286e5c1671a4694f453339f3aef3b30
Sunnatilla Ismoilov <sunnatilla.ismailov@gmail.com>
2024-05-13 05:31:56 +0300

    Update index.ts

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,10 +1,11 @@
 import { Nitro } from "nitropack";
 import mongoose from "mongoose";
+import { config } from "dotenv";
 
 export default async (_nitroApp: Nitro) => {
-  const config = useRuntimeConfig();
+  config();
   try {
-    await mongoose.connect(config.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     return { statusCode: 201 };
   } catch (error) {
     return { statusCode: 500 };
